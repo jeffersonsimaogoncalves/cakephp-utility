8a9d2ed8e29aa86716885a3e8498547456d3d942
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2018-06-08 18:57:50 -0300

    Update Plugin

---
+++ src/Plugin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jefferson Simão Gonçalves
+ * Email: gerson.simao.92@gmail.com
+ * GitHub: https://github.com/jeffersonsimaogoncalves
+ * Date: 08/06/2018
+ * Time: 16:49
+ */
+
+namespace JeffersonSimaoGoncalves\Utils;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Class Plugin
+ *
+ * @author Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
+ *
+ * @package JeffersonSimaoGoncalves\Utils
+ */
+class Plugin extends BasePlugin
+{
+    protected $routesEnabled = false;
+    protected $bootstrapEnabled = false;
+    protected $middlewareEnabled = false;
+    protected $consoleEnabled = false;
+}
